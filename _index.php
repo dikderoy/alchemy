5e5b05df84cecf80d68a08afd171620044015f4b
Bulgokov R.S. aka Deroy <dik.deroy@gmail.com>
2012-10-26 04:49:28 +0400

    Config class - first implementation
     some tests in _index.php

--- _index.php
+++ _index.php
@@ -3,9 +3,9 @@
 
 	function __autoload($class)
 	{
-		$path = $_SERVER['DOCUMENT_ROOT']."/class/";
-		$file = $path.$class.".php";
-		if(file_exists($file)) {
+		$path = $_SERVER['DOCUMENT_ROOT'] . "/class/";
+		$file = $path . $class . ".php";
+		if (file_exists($file)) {
 			include_once $file;
 		} else {
 			throw new Exception("can't autoload class - $class");
@@ -14,32 +14,64 @@
 
 	class TestModel extends ObjectModel
 	{
-		public $data;
 
+		public $data;
 		protected $db_table = 'test';
 		protected $db_fields = array(
 			'id' => 'isValidObjectId',
 			'name' => 'isValidObjectName',
 			'data' => ''
 		);
+
 	}
 
 	try {
-		$dbh = Db::getInstance();
-		$dbh->setDbParameters('mysql', 'mmvc.ops', 'test', 'utf8', 'mysql', 'mysql');
-		var_dump($dbh);
+		$a = array(
+			// string - defines server address
+			'db_server' => 'mmvc.ops',
+			// string - defines used db driver (PDO)
+			'db_driver' => 'mysql',
+			// string - defines DB name
+			'db_name' => 'test',
+			// string - defines DB login
+			'db_login' => 'mysql',
+			// string - defines DB password
+			'db_password' => 'mysql',
+			// string - defines used charset for DBConnection
+			'db_charset' => 'utf8',
+			// string - defines used charset for HTML
+			'site_encoding' => 'utf-8',
+			// string - defines used DOCTYPE
+			'html_doctype' => 'html5',
+			// bool - defines access_control enabled on main page or not
+			'main_access_restricted' => FALSE,
+			// bool - defines whatever debug info (post, get, session, cookie arrays print_r()) must be shown or not
+			'show_enveronment_debug' => FALSE,
+			// bool - defines whatever debug var_dump() function executed on response data
+			'show_response_vardump' => TRUE,
+			// int - define a lifetime of cookies in seconds
+			'cookies_lifetime' => 40000,
+		);
 
-		$obj = new TestModel(1);
-		var_dump($obj);
-		$obj->data = uniqid();
-		$obj->save();
+		$c = new Config($a);
+
+		$dbh = Db::getInstance();
+		$dbh->setDbParameters($c->db_driver, $c->db_server, $c->db_name, $c->db_charset, $c->db_login, $c->db_password);
+		//var_dump($dbh);
+
+		/*
+		  $obj = new TestModel(1);
+		  var_dump($obj);
+		  $obj->data = uniqid();
+		  $obj->save();
+		 */
 
 
 		$reloaded_obj = new TestModel(1);
 		var_dump($reloaded_obj);
 	} catch (Exception $exc) {
-			echo $exc->getTraceAsString();
-			echo $exc->getMessage();
+		echo $exc->getTraceAsString();
+		echo $exc->getMessage();
 	}
 
 ?>
